81628213ed7b30a8122900e85a5a35a2330c5b5e
Mahtte <530521@student.hin.no>
2015-11-16 02:19:15 +0100

    added tests for bitToInt method. created the method and started writing code

--- TDD/src/Utility.java
+++ TDD/src/Utility.java
@@ -1,14 +1,35 @@
-
 public class Utility {
 
 	public static int convertBitToInt(String string) {
+		/**
+		 * Check if string has more than 24 characters and if string contains
+		 * other than 0 and 1
+		 */
+		if (string.length() > 24) {
+			throw new IllegalArgumentException(
+					"String length must be less than 24");
+		}
+
+		if (hasLetter(string) && string.length() > 0) {
+			throw new IllegalArgumentException(
+					"String can only contain numbers 0 - 1");
+		}
+
+		/** Convert from bit to int */
 		int converted = 0;
 		if (string.length() < 1) {
 			return converted;
-		}
-		else
+		} else
 			return converted;
-		
+
 	}
-//
+
+	private static boolean hasLetter(String string) {
+		String regex = "\\d+";
+		if (string.matches(regex)) {
+			return false;
+		} else
+			return true;
+
+	}
 }
--- TDD/test/TestUtility.java
+++ TDD/test/TestUtility.java
@@ -1,13 +1,49 @@
 import static org.junit.Assert.*;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 
 public class TestUtility {
+	
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
 
 	@Test
 	public void ConvertBitToInt_EmptyString_ShouldReturnZero() {
-		Utility.convertBitToInt("");
+		assertEquals(0, Utility.convertBitToInt(""));
 	}
+	
+	@Test
+	public void ConvertBitToInt_StringLengtIsMoreThan24_ShouldThrowException() {
+		exception.expect(IllegalArgumentException.class);
+		exception.expectMessage("String length must be less than 24");
+		Utility.convertBitToInt("00000000000000000000000000000");
+	}
+	
+	@Test
+	public void ConvertBitToInt_StringLengtIs24_ShouldNotThrowException() {
+		Utility.convertBitToInt("000000000000000000000000");
+	}
+	
+	@Test
+	public void ConvertBitToInt_StringContainsALetter_ShouldThrowException() {
+		exception.expect(IllegalArgumentException.class);
+		exception.expectMessage("String can only contain numbers 0 - 1");
+		Utility.convertBitToInt("01234567890k");
+	}
+	
+	@Test
+	public void ConvertBitToInt_StringContainsANumberBiggerThan1_ShouldThrowException() {
+		exception.expect(IllegalArgumentException.class);
+		exception.expectMessage("String can only contain numbers 0 - 1");
+		Utility.convertBitToInt("01234567890");
+	}
+	
+	
+	
+	
+	
 
 }
