88be17c526921d6b1e9259ea745b7f30f9791ccf
Mahtte <530521@student.hin.no>
2015-11-18 22:54:01 +0100

    removed a method that was not needed anymore

--- TDD/src/Utility.java
+++ TDD/src/Utility.java
@@ -12,7 +12,7 @@ public class Utility {
 		}
 		int converted = 0;
 		for (int i = 0; i < string.length(); i++) {
-			converted += charIsZeroOrOne(string.charAt(string.length() - 1 - i))
+			converted += getValue(string.charAt(string.length() - 1 - i))
 					* Math.pow(2, i);
 		}
 		return converted;
@@ -98,20 +98,6 @@ public class Utility {
 			return true;
 		} else
 			return false;
-	}
-
-	/**
-	 * converts character 1 or 0 to int value 1 or 0.
-	 * 
-	 * @param c
-	 *            character to be converted
-	 * @return 1 or 0
-	 */
-	private static int charIsZeroOrOne(char c) {
-		if (c == '1') {
-			return 1;
-		} else
-			return 0;
 	}
 
 	public static int convertHexToInt(String s) {
